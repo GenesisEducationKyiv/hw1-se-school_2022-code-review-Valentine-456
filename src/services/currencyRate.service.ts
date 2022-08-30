d75df8b8451f889624fb08ef409d56e2a1ef6196
Valentyn Bondarenko <murica.it@gmail.com>
2022-08-31 02:32:50 +0300

    Move the link to a 3rd-party api to .env + added typings for env variables

--- src/services/currencyRate.service.ts
+++ src/services/currencyRate.service.ts
@@ -3,13 +3,13 @@ import { request as fetch } from "undici";
 type rate = number | null;
 
 class CurrencyRateSerivice {
-  private static readonly url: string =
-    "https://pro-api.coinmarketcap.com/v2/tools/price-conversion?amount=1&symbol=BTC&convert=UAH";
+  private static readonly url: string = process.env.CURRENCY_API_URL;
+  private static readonly api_key: string = process.env.CURRENCY_API_KEY;
 
   static async getRate(): Promise<rate> {
     const { statusCode, body } = await fetch(this.url, {
       headers: {
-        "X-CMC_PRO_API_KEY": process.env.CURRENCY_API_KEY,
+        "X-CMC_PRO_API_KEY": this.api_key,
       },
     });
     if (statusCode == 200) {
